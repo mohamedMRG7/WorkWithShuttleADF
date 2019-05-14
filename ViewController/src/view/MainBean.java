6988788ec1e7d87d9606702199050534cd17566f
Mohamed Ragab <devmohamed1325@gmail.com>
2019-05-14 12:42:48 +0200

    Add role finished

--- ViewController/src/view/MainBean.java
+++ ViewController/src/view/MainBean.java
@@ -3,6 +3,9 @@ package view;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.ViewHandler;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
@@ -10,6 +13,10 @@ import javax.faces.model.SelectItem;
 import oracle.adf.model.BindingContext;
 import oracle.adf.model.binding.DCBindingContainer;
 import oracle.adf.model.binding.DCIteratorBinding;
+
+import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
+
+import oracle.adf.view.rich.context.AdfFacesContext;
 
 import oracle.binding.AttributeBinding;
 
@@ -21,9 +28,20 @@ import oracle.jbo.domain.Number;
 public class MainBean {
     List<Integer> sItems;
     List allItems;
-    String x="";
+    boolean enableAddRole = false;
+    private RichPanelFormLayout addRoleForm;
+
     public MainBean() {
-        
+
+    }
+
+
+    public void setEnableAddRole(boolean enableAddRole) {
+        this.enableAddRole = enableAddRole;
+    }
+
+    public boolean isEnableAddRole() {
+        return enableAddRole;
     }
 
     public void setSItems(List sItems) {
@@ -31,8 +49,8 @@ public class MainBean {
     }
 
     public List getSItems() {
-        if(sItems==null)
-        sItems=getRolesForEmp();
+        if (sItems == null)
+            sItems = getRolesForEmp();
         return sItems;
     }
 
@@ -41,89 +59,122 @@ public class MainBean {
     }
 
     public List getAllItems() {
-        if(allItems==null)
-            allItems=getAllRolesFromItert();
-        
+        if (allItems == null)
+            allItems = getAllRolesFromItert();
+
         return allItems;
     }
 
-    
-   
 
-    public List<SelectItem> getAllRolesFromItert()
-    {
-        DCIteratorBinding roleIterator=getIterator("RolesVOIterator");
+    public List<SelectItem> getAllRolesFromItert() {
+        DCIteratorBinding roleIterator = getIterator("RolesVOIterator");
         List<SelectItem> selectItems = new ArrayList<SelectItem>();
         for (Row row : roleIterator.getAllRowsInRange()) {
-            selectItems.add(new SelectItem(row.getAttribute("RoleId"),
-                                           (String)row.getAttribute("Role")));
+            selectItems.add(new SelectItem(row.getAttribute("RoleId"), (String) row.getAttribute("Role")));
         }
         return selectItems;
     }
-    
-    public List<Integer> getRolesForEmp()
-    {
-      DCIteratorBinding iter=getIterator("EmpRolesVOIterator");
-      List<Integer> rolesForCurrentEmp = new ArrayList<Integer>();
+
+    public List<Integer> getRolesForEmp() {
+        DCIteratorBinding iter = getIterator("EmpRolesVOIterator");
+        List<Integer> rolesForCurrentEmp = new ArrayList<Integer>();
         for (Row row : iter.getAllRowsInRange()) {
-            rolesForCurrentEmp.add((Integer)row.getAttribute("RoleId"));
+            rolesForCurrentEmp.add((Integer) row.getAttribute("RoleId"));
         }
-    return rolesForCurrentEmp;
+        return rolesForCurrentEmp;
     }
-    
-    public DCIteratorBinding getIterator(String iteratorID)
-    {
-            BindingContext ctx=BindingContext.getCurrent();
-            DCBindingContainer bc = (DCBindingContainer) ctx.getCurrentBindingsEntry();
-            DCIteratorBinding iter = bc.findIteratorBinding(iteratorID);
-            return iter;
-        }
+
+    public DCIteratorBinding getIterator(String iteratorID) {
+        BindingContext ctx = BindingContext.getCurrent();
+        DCBindingContainer bc = (DCBindingContainer) ctx.getCurrentBindingsEntry();
+        DCIteratorBinding iter = bc.findIteratorBinding(iteratorID);
+        return iter;
+    }
+
     public void onChange(ValueChangeEvent valueChangeEvent) {
         // Add event code here...
-        if(valueChangeEvent.getOldValue()==valueChangeEvent.getNewValue())
-        return;
-        
+        if (valueChangeEvent.getOldValue() == valueChangeEvent.getNewValue())
+            return;
+
         sItems = (ArrayList) valueChangeEvent.getNewValue();
-        
-       
+
+
     }
-    
-    
+
+
     public Integer getCurrentEmpId() {
-        BindingContext ctx=BindingContext.getCurrent();
+        BindingContext ctx = BindingContext.getCurrent();
         DCBindingContainer bc = (DCBindingContainer) ctx.getCurrentBindingsEntry();
-        AttributeBinding attr = (AttributeBinding)bc.findCtrlBinding("EmpId");
-        Integer empId = (Integer)attr.getInputValue();
+        AttributeBinding attr = (AttributeBinding) bc.findCtrlBinding("EmpId");
+        Integer empId = (Integer) attr.getInputValue();
         return empId;
     }
+
     public void saveChanges(ActionEvent actionEvent) {
         // Add event code here...
-        DCIteratorBinding iter=getIterator("EmpRolesVOIterator");
-        for(Row row:iter.getAllRowsInRange())
+        DCIteratorBinding iter = getIterator("EmpRolesVOIterator");
+        for (Row row : iter.getAllRowsInRange())
             row.remove();
-        
-        for(Integer roleID :sItems){
-            Row row=iter.getRowSetIterator().createRow();
+
+        for (Integer roleID : sItems) {
+            Row row = iter.getRowSetIterator().createRow();
             row.setAttribute("EmpId", getCurrentEmpId());
             row.setAttribute("RoleId", roleID);
             iter.getRowSetIterator().insertRow(row);
-            }
-        
-       excuteOperation("Commit");
-        
-    }
-    
-    public void excuteOperation(String operationID)
-    {
-            BindingContext ctx=BindingContext.getCurrent();
-            DCBindingContainer bc = (DCBindingContainer) ctx.getCurrentBindingsEntry();
-            OperationBinding operation = bc.getOperationBinding(operationID);
-            operation.execute();
         }
+
+        excuteOperation("Commit");
+
+    }
+
+    public void excuteOperation(String operationID) {
+        BindingContext ctx = BindingContext.getCurrent();
+        DCBindingContainer bc = (DCBindingContainer) ctx.getCurrentBindingsEntry();
+        OperationBinding operation = bc.getOperationBinding(operationID);
+        operation.execute();
+    }
 
     public String resetSelectedList() {
         // Add event code here...
         setSItems(null);
         return null;
+    }
+
+    public void addRole(ActionEvent actionEvent) {
+        // Add event code here...
+        setEnableAddRole(true);
+       refreshPage();
+
+    }
+
+    public void setAddRoleForm(RichPanelFormLayout addRoleForm) {
+        this.addRoleForm = addRoleForm;
+    }
+
+    public RichPanelFormLayout getAddRoleForm() {
+        return addRoleForm;
+    }
+
+    protected void refreshPage() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        String refreshpage = fc.getViewRoot().getViewId();
+        ViewHandler ViewH = fc.getApplication().getViewHandler();
+        UIViewRoot UIV = ViewH.createView(fc, refreshpage);
+        UIV.setViewId(refreshpage);
+        fc.setViewRoot(UIV);
+    }
+
+    public void addRole_action(ActionEvent actionEvent) {
+        // Add event code here...
+        excuteOperation("Commit");
+        setEnableAddRole(false);
+        allItems = getAllRolesFromItert();
+        refreshPage();
+    }
+
+    public String toManySelect() {
+        // Add event code here...
+        setEnableAddRole(false);
+        return "next";
     }
 }
